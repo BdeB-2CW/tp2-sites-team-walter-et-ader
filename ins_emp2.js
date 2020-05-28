1417383142a76cbf01098af8e9385968c2ba84fd
Ader Alisma <62966872+Adeder22@users.noreply.github.com>
2020-05-28 15:37:32 -0400

    Script Insert fonctionnel

--- ins_emp2.js
+++ ins_emp2.js
@@ -1,18 +1,12 @@
-function ins_empl() {
+function ins_membre() {
   let membr_rec = {
-    membre_id: 1110,
-    nom: "DitBonsoir",
-    prenom: "Ader",
-    Ville: "Laval",
-    email: "eggpl4nt@hotmail.fr",
-    mot_de_passe: "4nt1vaxx",
     points: 0,
   };
   vide=false;
   membr_rec.membre_id =Math.floor((Math.random() * 9998) + 1);
   membr_rec.nom = document.getElementById("nom").value;
   membr_rec.prenom = document.getElementById("prenom").value;
-  membr_rec.Ville = document.getElementById("ville").value;
+  membr_rec.ville = document.getElementById("ville").value;
   membr_rec.email = document.getElementById("email").value;
   membr_rec.mot_de_passe = document.getElementById("mdp").value;
   //Afficher un message d'alerte si une des cases sont vides, sinon, envoie les données vers la BD
@@ -21,7 +15,7 @@ if(membr_rec.nom===""||membr_rec.prenom===""||membr_rec.Ville===""||membr_rec.em
 }else{
 
   const URL =
-    "https://dkearjhlg7gwib7-db202005071430.adb.ca-montreal-1.oraclecloudapps.com/ords/tp2/membre";
+    "Access-Control-Allow-Origin:https://dkearjhlg7gwib7-db202005071430.adb.ca-montreal-1.oraclecloudapps.com/ords/tp2/membre";
 
   fetch(URL, {
     method: "POST",
