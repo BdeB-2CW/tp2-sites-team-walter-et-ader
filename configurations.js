c2d8545af128e2683005c9283e3279d3dab616a5
ZanderLavastory <zanderlavastory@gmail.com>
2020-05-30 23:31:23 -0400

    Update configurations.js

--- configurations.js
+++ configurations.js
@@ -6,20 +6,37 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-const image = document.getElementById("image");
+const nomPizza = document.getElementById("nom");
+const imgPizza = document.getElementById("imagePizza");
+conf = document.getElementById("conf1");
 //const URL = "http://localhost:8080/ords/hr2/employees"; si la bd est locale
 const URL =
-"https://dkearjhlg7gwib7-db202005071430.adb.ca-montreal-1.oraclecloudapps.com/ords/wtp2/pizza/"
+"https://dkearjhlg7gwib7-db202005071430.adb.ca-montreal-1.oraclecloudapps.com/ords/wtp2/piz_vue/"
 fetch(URL)
   .then((resp) => resp.json())
   .then(function (data) {
-    let pizza = data.items; //.results;
-    return pizza.map(function (pizza) {
-      if(pizza.pizza_id === 685){     
-                                    let img = createNode("img");
-                                    img.src = `${pizza.source_image}`;
-                                    append(image, img);
-                                  }
+    let piz_vue = data.items; //.results;
+    
+    return piz_vue.map(function (pizza) {
+      let noms = ["Garnitures: ", "Emplacement: ", "Quantité :" ]
+      let elements = [pizza.g_nom, pizza.emplacement, pizza.quantité]
+      if(pizza.pizza_id === 685){   
+
+                                    imgPizza.src = `${pizza.source_image}`;
+                                    nomPizza.innerHTML = `${pizza.p_nom}`;
+                                    for(var i = 0; i < noms.length; i++){
+                                      li = createNode("li");
+                                      h2 = createNode("h2");
+                                      
+                                      h2.innerHTML = noms[i] + `${elements[i]}`
+                                      
+                                      append(li, h2);
+                                      append(conf, li);
+                                    }
+                                    conf = document.getElementById("conf2");
+
+
+                                    }
     });
   })
   .catch(function (error) {
