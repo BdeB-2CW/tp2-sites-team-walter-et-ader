4f25dd7563074d6ff159408ce62c517f81adc55b
ZanderLavastory <zanderlavastory@gmail.com>
2020-06-01 03:41:00 -0400

    fichiersSQL

---
+++ fichiersSQL/insert_garniture.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - Monday-June-01-2020   
+--------------------------------------------------------
+REM INSERTING into RESTSCOTT3.GARNITURE
+SET DEFINE OFF;
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (154,'Champignon',2,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (458,'Onion',2,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (875,'Poivron',3,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (119,'Bacon',4,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (256,'Jalapeno',3,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (598,'Epinard',2,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (654,'Olives Vertes',2,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (235,'Jambon',2,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (623,'Saucisse',2,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (435,'Broccoli',3,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (357,'Cheddar',4,null);
+Insert into RESTSCOTT3.GARNITURE (GARNITURE_ID,NOM,PRIX,DESCRIPTION) values (100,'Ananas',4,null);
---
+++ fichiersSQL/insert_pizza.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Monday-June-01-2020   
+--------------------------------------------------------
+REM INSERTING into RESTSCOTT3.PIZZA
+SET DEFINE OFF;
+Insert into RESTSCOTT3.PIZZA (PIZZA_ID,NOM,PRIX,RABAIS,DESCRIPTION,SOURCE_IMAGE) values (685,'Regal_animal',14,100,'Pizza introduisant le nouvel ingredient: os','./images/regal.jpeg');
+Insert into RESTSCOTT3.PIZZA (PIZZA_ID,NOM,PRIX,RABAIS,DESCRIPTION,SOURCE_IMAGE) values (415,'Peuplulaire',11,100,'La classique: pepperoni, fromage, pas le temps de niaiser','./images/peuplulaire.png');
+Insert into RESTSCOTT3.PIZZA (PIZZA_ID,NOM,PRIX,RABAIS,DESCRIPTION,SOURCE_IMAGE) values (454,'Végétarienne',11,100,'Le préféré des jardiniers: champignons, poivrons, olives noires et ananas','./images/vegetarienne.png');
+Insert into RESTSCOTT3.PIZZA (PIZZA_ID,NOM,PRIX,RABAIS,DESCRIPTION,SOURCE_IMAGE) values (964,'Ram Ranch',14,100,'Plaisir coupable: bacon, poulet, pepperoni. Le tout: E X T R A.','./images/ram.png');
+Insert into RESTSCOTT3.PIZZA (PIZZA_ID,NOM,PRIX,RABAIS,DESCRIPTION,SOURCE_IMAGE) values (168,'Sud-America',14,100,'Soy muy delicioso con olives y poivrons y champignons.','./images/sud.jpg');
